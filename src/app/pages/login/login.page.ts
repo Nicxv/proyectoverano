aade99432088b4ad5f817e36c5a5f74a9ef6175a
Nicxv <118311129+Nicxv@users.noreply.github.com>
2024-01-04 13:14:33 -0300

    Modificaciones del home  y login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario: string ="";
+  clave: string ="";
+
+  userCorrecto: string ="Admin";
+  claveCorrecta: string ="12345";
+
+  constructor(private alertController: AlertController, private toastController: ToastController, private router: Router) { }
+
+
+  login(){
+    if(this.usuario == this.userCorrecto && this.clave == this.claveCorrecta){
+      this.presentAlert("Bienvenido");
+      this.router.navigate(['/']);
+    }else{
+      this.presentAlert("Usuario y/o Clave incorrecta");
+    }
+  }
+
+  async presentAlert(msj:string) {
+    const alert = await this.alertController.create({
+      header: 'Importante',
+      message: msj,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {
   }
-
+ 
 }
