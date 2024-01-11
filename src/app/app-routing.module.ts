79a3efd7cd4b6502848e8859ce2ab04f36810f02
Nicxv <118311129+Nicxv@users.noreply.github.com>
2024-01-11 01:52:45 -0300

    jj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -92,9 +92,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/perfil/perfil.module').then( m => m.PerfilPageModule)
   },
 
-  /* La de abajo es la pagina de error, siempre debe ir al ultimo,
-   cuando creen una pagina, deben mover la recien creada arriba de la pag de error*/
- 
   {
     path: 'agregar',
     loadChildren: () => import('./pages/agregar/agregar.module').then( m => m.AgregarPageModule)
@@ -109,7 +106,8 @@ const routes: Routes = [
   },
 
   {
-
+/* La de abajo es la pagina de error, siempre debe ir al ultimo,
+   cuando creen una pagina, deben mover la recien creada arriba de la pag de error*/
     path: '**',
     loadChildren: () => import('./pages/pagina3/pagina3.module').then( m => m.Pagina3PageModule)
   },
