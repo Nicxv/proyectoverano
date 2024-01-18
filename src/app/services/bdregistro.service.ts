8c33e3427a91c11da40c7d74037bb902f1825114
elxumare1 <nico0valenzuelacl@gmail.com>
2024-01-18 16:47:29 -0300

    b

--- src/app/services/bdregistro.service.ts
+++ src/app/services/bdregistro.service.ts
@@ -16,7 +16,7 @@ export class BdregistroService {
 
   //variables para las tablas de nuestra base de datos
   tablaRol: string = "CREATE TABLE IF NOT EXISTS rol(id_rol INTEGER PRIMARY KEY autoincrement, nombre VARCHAR(20) NOT NULL);";
-  tablaUsuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario INTEGER PRIMARY KEY autoincrement, correo VARCHAR(100) NOT NULL, clave VARCHAR(16) NOT NULL, nombre VARCHAR(50), apellido VARCHAR(50), telefono VARCHAR(12), foto blob, fk_id_rol INTEGER, FOREIGN KEY(fk_id_rol) REFERENCES rol(id_rol));";
+  tablaUsuario: string = "CREATE TABLE IF NOT EXISTS usuario(id_usuario INTEGER PRIMARY KEY autoincrement, correo VARCHAR(100) NOT NULL, clave VARCHAR(16) NOT NULL, nombreu VARCHAR(50), apellido VARCHAR(50), telefono VARCHAR(12), foto blob, fk_id_rol INTEGER, FOREIGN KEY(fk_id_rol) REFERENCES rol(id_rol));";
 
   tablaCategoria: string = "CREATE TABLE IF NOT EXISTS categoria(id_categoria INTEGER PRIMARY KEY autoincrement, nombre VARCHAR(20) NOT NULL);";
   tablaProducto: string = "CREATE TABLE IF NOT EXISTS producto(id_producto INTEGER PRIMARY KEY autoincrement, nombrep VARCHAR(30) NOT NULL, descripcion VARCHAR(100) NOT NULL, stock INTEGER NOT NULL, precio INTEGER NOT NULL, foto BLOB NOT NULL , fk_id_categoria INTEGER, FOREIGN KEY(fk_id_categoria) REFERENCES categoria(id_categoria));";
@@ -223,9 +223,9 @@ export class BdregistroService {
   }
 
  
-  insertarUsuario(nombre:string, apellido: string, correo:string, telefono:string, clave:string, foto: any,fk_id_rol:number){
+  insertarUsuario(nombreu:string, apellido: string, correo:string, telefono:string, clave:string, foto: any,fk_id_rol:number){
     //ejecutamos el insert
-    return this.conexionBD.executeSql('INSERT INTO usuario(nombre, apellido, correo, telefono, clave, foto, fk_id_rol) VALUES (?,?,?,?,?,?,?)',[nombre, apellido, correo, telefono, clave, foto, fk_id_rol]).then(res=>{
+    return this.conexionBD.executeSql('INSERT INTO usuario(nombreu, apellido, correo, telefono, clave, foto, fk_id_rol) VALUES (?,?,?,?,?,?,?)',[nombreu, apellido, correo, telefono, clave, foto, fk_id_rol]).then(res=>{
       //this.buscarUsuarios();
       this.presentAlert("Usuario Registrado!");
       this.router.navigate(['/login']);
