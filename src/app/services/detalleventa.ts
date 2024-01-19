43af1677e18f3769a852d5b6415067419cc5bebe
Alv4r0o0o0 <107275107+Alv4r0o0o0@users.noreply.github.com>
2024-01-19 20:37:44 -0300

    aaa

---
+++ src/app/services/detalleventa.ts
@@ -0,0 +1,7 @@
+export class Detalleventa {
+    id_detalle: Number =0;
+    id_venta: number =0;
+    id_producto: number = 0;
+    cantidad: number =0;
+    subtotal: Number=0;
+}
---
+++ src/app/services/venta.ts
@@ -0,0 +1,10 @@
+export class Venta {
+
+    id_venta: number =0;
+    total: number =0;
+    f_venta!: Date;
+    estado: string ="";
+    id_usuario: number = 0;
+
+
+}
